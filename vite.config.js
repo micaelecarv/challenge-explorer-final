b3d0d820320482115a3e6ffc63ffa0a6a30abc1d
Micaele Carvalho <micaelecarv@gmail.com>
2024-01-05 09:55:32 -0300

    testing

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/challenge-explorer-final/',
-  server: {
-    port: 3000,
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-      "@tests": path.resolve(__dirname, "./tests")
-    },
-  },
 })
