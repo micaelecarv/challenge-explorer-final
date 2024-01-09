2ed6a5678647964c9e4fcaae437984a49fb2fe72
Micaele Carvalho <micaelecarv@gmail.com>
2024-01-09 15:40:48 -0300

    testing datadog v3

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,25 @@
-import React from "react"
-import { datadogRum } from '@datadog/browser-rum';
-
-
-datadogRum.init({
-  applicationId: '2689cbf3-e6f1-4fb0-b44d-2820625ed874',
-  clientToken: 'pub60cb0a778bce627ea968188d42a939c0',
-  site: 'datadoghq.com',
-  service: 'challenge-explorer-final',
-  env: 'production',
-  version: '1.0.0', 
-  sessionSampleRate: 100,
-  sessionReplaySampleRate: 100,
-  trackUserInteractions: true,
-  trackResources: true,
-  trackLongTasks: true,
-  defaultPrivacyLevel: 'mask-user-input',
-});
-
-datadogRum.startSessionReplayRecording();
+import React, { useEffect } from "react";
+import { datadogRum } from "@datadog/browser-rum";
+import { datadogConfig } from "./datadog.config";
 
 export function App() {
+  useEffect(() => {
+    datadogRum.init(datadogConfig);
+    datadogRum.startSessionReplayRecording();
+  }
+  , []);
+
   function handleClick() {
-    console.log('Button clicked');
+    console.log("Button clicked");
   }
   return (
     <>
-     <h1>Mica</h1>
-     <button onClick={handleClick}>Clique aqui</button>
-     <input type="text" placeholder="Digite aqui" />
-     <div>
+      <h1>Mica</h1>
+      <button onClick={handleClick}>Clique aqui</button>
+      <input type="text" placeholder="Digite aqui" />
+      <div>
         <input type="checkbox" id="scales" name="scales" checked />
       </div>
     </>
-  )
-};
+  );
+}
---
+++ src/datadog.config.ts
@@ -0,0 +1,16 @@
+import { DefaultPrivacyLevel } from "@datadog/browser-rum";
+
+export const datadogConfig = {
+    applicationId: "2689cbf3-e6f1-4fb0-b44d-2820625ed874",
+    clientToken: "pub60cb0a778bce627ea968188d42a939c0",
+    site: "datadoghq.com",
+    service: "challenge-explorer-final",
+    env: "production",
+    version: "2.0.0",
+    sessionSampleRate: 100,
+    sessionReplaySampleRate: 100,
+    trackUserInteractions: true,
+    trackResources: true,
+    trackLongTasks: true,
+    defaultPrivacyLevel: "mask-user-input" as DefaultPrivacyLevel,
+}
